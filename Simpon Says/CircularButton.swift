11c2d13a2ee49a54d1b5a2a3cfd86e349f5ca3d8
LacySimpson <lacy.simpson@usm.edu>
2018-05-15 15:14:28 -0500

    first commit

---
+++ Simpon Says/CircularButton.swift
@@ -0,0 +1,27 @@
+//
+//  CircularButton.swift
+//  Simpon Says
+//
+//  Created by Lacy Simpson on 5/15/18.
+//  Copyright © 2018 Lacy Simpson. All rights reserved.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.width/2
+        layer.masksToBounds = true
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                alpha = 1.0
+            } else {
+                alpha = 0.5
+            }
+        }
+    }
+}
