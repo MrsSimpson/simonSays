41463eba4dbb5047246521c31cf6ab065ba6a8b2
LacySimpson <lacy.simpson@usm.edu>
2018-05-15 12:33:10 -0500

    Initial Commit

---
+++ Simpon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simpon Says
+//
+//  Created by Lacy Simpson on 5/15/18.
+//  Copyright © 2018 Lacy Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
